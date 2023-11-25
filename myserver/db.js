8de7f7128760676434dbb35741ab7fa56dc18ab5
ashutosh negi <ashutoshnegi195@gmail.com>
2023-11-25 18:31:46 +0530

    environment variable

--- myserver/db.js
+++ myserver/db.js
@@ -1,15 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "question",
-    password: "Negi@1234",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
 });
 
 conn.connect((err) => {
-    if(err) throw err;
-    console.log("connected to database");
+  if (err) throw err;
+  console.log("connected to database");
 });
 
 module.exports = conn.promise();
